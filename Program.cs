a600e393a1801f9b67de200da008a969cf2c0f7a
Pratama-Raka <pratamapoetra14@gmail.com>
2026-04-15 07:07:50 +0700

    menambah class FilmFavorit_103022400053

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul7_103022300053;
+
+FilmFavorit_103022400053 fff = new FilmFavorit_103022400053();
+fff.ReadJson();
