00227bbb9dfde36a3068af19b15aaf01269e2c4f
Syamsicon <faliq.aqil.ra@gmail.com>
2026-04-15 07:41:45 +0700

    menambahkan class filmfavorit_103022400088

---
+++ FilmFavorit_103022400088.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7_103022300053
+{
+    public class FilmFavorit_103022400088
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public string rating { get; set; }
+        public string duration { get; set; }
+
+        public bool isWatched { get; set; }
+    }
+
+    public class showFilm
+    {
+        public void readJSON()
+        {
+            string path = "D:\\New folder\\Modul7_Goblin_Salto\\jurnal7_1_103022400088.json";
+            string jsonString = File.ReadAllText(path);
+            FilmFavorit_103022400088 film = JsonSerializer.Deserialize<FilmFavorit_103022400088>(jsonString);
+            Console.WriteLine($"Film Berjudul {film.title} yang disutradarai oleh {film.director} dan terbit di tahun {film.year} dengan genre {film.genre}. film ini memiliki rating {film.rating} yang berdurasi {film.duration} dan saya sudah menonton: {film.isWatched} ");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// See https://aka.ms/new-console-template for more information
+using Modul7_103022300053;
+
+showFilm film = new showFilm();
+film.readJSON();
