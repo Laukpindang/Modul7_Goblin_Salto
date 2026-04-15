0e40d895d6c98f19dd1fb16975e5fae3d2899f5a
Mri-cia <vyarnamricia@gmail.com>
2026-04-15 07:52:01 +0700

    menambahkan class GenreDictionary_103022400106

--- Program.cs
+++ Program.cs
@@ -5,9 +5,14 @@ Console.WriteLine();
 //Rawikara
 var filmFavorit = new FilmFavorit_103022400106();
 var watchlist = new Watchlist_103022400106();
+var genreDictionary = new GenreDictionary_103022400106();
 
 filmFavorit.readJson();
 
 Console.WriteLine("\n");
 
 watchlist.readJson();
+
+Console.WriteLine("\n");
+
+genreDictionary.readJson();
