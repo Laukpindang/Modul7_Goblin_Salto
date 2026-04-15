2524cc6cf2e32da81b4b4c675bc2f7f69ca8a153
Mri-cia <vyarnamricia@gmail.com>
2026-04-15 07:16:33 +0700

    menambahkan class FilmFavorit_103022400106

---
+++ FilmFavorit_103022400106.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Modul7_103022400106
+{
+    public class FilmFavorit_103022400106
+    {
+        [JsonIgnore]
+        private string _filePath = Path.Combine(Environment.CurrentDirectory, "jurnal7_1_103022400106.json");
+
+        public class Film
+        {
+            [JsonPropertyName("title")]
+            public string Title { get; set; }
+            [JsonPropertyName("director")]
+            public string Director { get; set; }
+            [JsonPropertyName("year")]
+            public string Year { get; set; }
+            [JsonPropertyName("genre")]
+            public string Genre { get; set; }
+            [JsonPropertyName("rating")]
+            public string Rating { get; set; }
+            [JsonPropertyName("durationMinutes")]
+            public string Duration { get; set; }
+            [JsonPropertyName("isWatched")]
+            public bool IsWatched { get; set; }
+        }
+
+        public void readJson()
+        {
+            string jsonString = File.ReadAllText(_filePath);
+            Film film = JsonSerializer.Deserialize<Film>(jsonString);
+
+            Console.WriteLine($"Title: {film.Title} \nDirector: {film.Director} \nYear: {film.Year}\nGenre: {film.Genre}\nRating: {film.Genre}\nDuration: {film.Duration}\nIs Watched: {film.IsWatched}");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul7_103022400106;
+
+//Rawikara
+var filmFavorit = new FilmFavorit_103022400106();
+
+filmFavorit.readJson();
